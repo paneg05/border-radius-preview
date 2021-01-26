5fcb817ac0ab6d8f70c0cec7921764128e037f35
luan <aluncard6@gmail.com>
2021-01-26 19:47:29 -0300

    iniciando projeto

---
+++ js/aplicacao.js
@@ -0,0 +1,20 @@
+var altura=document.getElementById('altura').value
+var largura=document.getElementById('largura').value
+
+var topoDireito=document.getElementById('td').value
+var topoEsquerdo=document.getElementById('te').value
+var baseDireita=document.getElementById('bd').value
+var baseEsquerda=document.getElementById('be').value
+function start(){
+    
+}
+
+function verificacaoAltura(){
+    
+   
+
+    if (teste==false){
+        alert('entre apenas com números!!')
+    }
+}
+start()
